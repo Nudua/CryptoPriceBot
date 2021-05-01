01e94d258a4c5e75ef20aaabbf6d31d709455012
Tor Ramstad <tor.audun@gmail.com>
2021-05-01 08:48:40 +0200

    Add support for IPriceAnalyzer

--- BinanceWebSocketManager.cs
+++ BinanceWebSocketManager.cs
@@ -1,21 +1,51 @@
 ﻿using Binance.Net;
+using CryptoPriceBot.Analyzers;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CryptoPriceBot
 {
-    public class BinanceWebsocketManager
+    public class BinanceWebSocketManager
     {
+        private readonly BinanceSocketClient _client;
+        private readonly List<IPriceAnalyzer> _priceAnalyzers;
+
+        public BinanceWebSocketManager(List<IPriceAnalyzer> priceAnalyzers)
+        {
+            // Throw an exception if the list is null
+            _priceAnalyzers = priceAnalyzers ?? throw new ArgumentNullException(nameof(priceAnalyzers));
+            _client = new BinanceSocketClient();
+        }
+
         public async Task<bool> ConnectAndSubscribeAsync()
         {
-            var client = new BinanceSocketClient();
-
-            var connectResult = await client.Spot.SubscribeToSymbolTickerUpdatesAsync("BTCUSDT", (tickData) =>
+            // Iterate over a list of all the unique Symbols to watch for price changes
+            foreach (var symbol in _priceAnalyzers.Select(x => x.Symbol).Distinct())
             {
-                Console.WriteLine($"Last price for Bitcoin is: {tickData.LastPrice}");
-            });
+                // Subscribe to the WebSocket stream for the Symbol
+                var result = await _client.Spot.SubscribeToSymbolTickerUpdatesAsync(symbol, (tickData) =>
+                {
+                    foreach (var analyzer in _priceAnalyzers)
+                    {
+                        // Call update if the symbol matches the current subscription
+                        if (analyzer.Symbol == symbol)
+                        {
+                            analyzer.OnUpdate(tickData);
+                        }
+                    }
+                });
 
-            return connectResult.Success;
+                if (!result.Success)
+                {
+                    // If one of the subscriptions fail to connect, we just cleanup and return false
+                    await _client.UnsubscribeAll();
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
