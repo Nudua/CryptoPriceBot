b20d5e8fca761f120203263ac524a04bee77b737
Tor Ramstad <tor.audun@gmail.com>
2021-05-01 08:41:57 +0200

    Create IPriceAnalyzer interface

---
+++ Analyzers/IPriceAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using Binance.Net.Interfaces;
+
+namespace CryptoPriceBot.Analyzers
+{
+    public interface IPriceAnalyzer
+    {
+        // Each analyzer will be attached to a given symbol
+        string Symbol { get; }
+
+        void OnUpdate(IBinanceTick tick);
+    }
+}
