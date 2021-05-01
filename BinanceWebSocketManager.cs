7c6fb709a8f558f941b56c1c5f02f40a4396267b
Tor Ramstad <tor.audun@gmail.com>
2021-05-01 08:37:18 +0200

    Create BinanceWebSocketManager.cs

---
+++ BinanceWebSocketManager.cs
@@ -0,0 +1,21 @@
+﻿using Binance.Net;
+using System;
+using System.Threading.Tasks;
+
+namespace CryptoPriceBot
+{
+    public class BinanceWebsocketManager
+    {
+        public async Task<bool> ConnectAndSubscribeAsync()
+        {
+            var client = new BinanceSocketClient();
+
+            var connectResult = await client.Spot.SubscribeToSymbolTickerUpdatesAsync("BTCUSDT", (tickData) =>
+            {
+                Console.WriteLine($"Last price for Bitcoin is: {tickData.LastPrice}");
+            });
+
+            return connectResult.Success;
+        }
+    }
+}
