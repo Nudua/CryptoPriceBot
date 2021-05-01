879c13f9dd9c61d0227c0438ef68a544879f6626
Tor Ramstad <tor.audun@gmail.com>
2021-05-01 08:41:34 +0200

    Update Program.cs to use our BinanceWebSocketManager

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using CryptoPriceBot;
+using System;
 
-namespace CryptoPriceBot
+Console.WriteLine("Starting bot...");
+
+var manager = new BinanceWebsocketManager();
+
+// Try connecting and subscribing for updates for Bitcoin price updates
+if (!await manager.ConnectAndSubscribeAsync())
 {
-    internal class Program
+    Console.WriteLine("Unable to connect to websocket, quitting...");
+    return;
+}
+
+// Keep the bot running until the user presses the 'q' key
+while (true)
+{
+    var keyInfo = Console.ReadKey();
+
+    if (keyInfo.Key == ConsoleKey.Q)
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        break;
     }
 }
