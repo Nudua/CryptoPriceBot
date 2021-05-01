6e7f426562fdd92da22b58cede6327e04a3ccf51
Tor Ramstad <tor.audun@gmail.com>
2021-05-01 09:03:23 +0200

    Add support for notification services

--- Analyzers/PriceAboveAnalyzer.cs
+++ Analyzers/PriceAboveAnalyzer.cs
@@ -1,29 +1,43 @@
 ﻿using Binance.Net.Interfaces;
+using CryptoPriceBot.Notifications;
 using System;
+using System.Collections.Generic;
 
 namespace CryptoPriceBot.Analyzers
 {
     public class PriceAboveAnalyzer : IPriceAnalyzer
     {
+        private bool _hasFired = false;
+        private readonly List<INotificationService> _services;
+
         private readonly decimal _abovePrice;
 
         // Implement the symbol property because the manager needs to know which symbols to subscribe to
         public string Symbol { get; }
 
-        public PriceAboveAnalyzer(string symbol, decimal abovePrice)
+        public PriceAboveAnalyzer(string symbol, decimal abovePrice, List<INotificationService> services)
         {
             Symbol = symbol;
             _abovePrice = abovePrice;
+            // New
+            _services = services ?? throw new ArgumentNullException(nameof(services));
         }
 
         public void OnUpdate(IBinanceTick tick)
         {
-            // For now let's just write to the console once our condition is met
-            // We'll change this to send an email in the next section once
-            // we've implemented a notification service for it
-            if (tick.LastPrice > _abovePrice)
+            if (!_hasFired && tick.LastPrice > _abovePrice)
             {
-                Console.WriteLine($"The price of {Symbol} is above {_abovePrice}! Price: {tick.LastPrice}");
+                _hasFired = true;
+
+                var message = $"The price of {Symbol} is above {_abovePrice}! Price: {tick.LastPrice}";
+
+                foreach (var service in _services)
+                {
+                    // Todo: Verify that the email was actually sent.
+                    service.Notify(message);
+                }
+
+                Console.WriteLine(message);
             }
         }
     }
